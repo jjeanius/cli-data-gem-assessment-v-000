f384fab1bce44c6d045706ade8cdc872edba0c50
Judy McGoldrick <jjeanius@verizon.net>
2018-03-10 04:26:34 +0000

    Done.

--- chefilicious/lib/chefilicious/cli.rb
+++ chefilicious/lib/chefilicious/cli.rb
@@ -2,11 +2,59 @@ class Chefilicious::CLI
 
   def call
     puts "Welcome to the Chefilicious!"
-    puts "What are you looking for? "
-    meal_type
-    cooking_time
-    allergens
+    puts "What are you looking for?"
+    list_meal_type
+    puts "Please select your desire cooking time"
+    list_cooking_time
+    puts "Are you allergic to any of the following ingredientes?"
+    list_allergens
+    puts "Please select a cuisine"
+    list_cuisine
+    puts "What do you like to cook?"
+    list_food_category
+    display_meal_kits
+    puts "Please make your selection"
+    display_meal_details
+
   end
+
+  def meal_type
+
+  end
+
+  def cooking_time
+
+  end
+
+  def list_allergens
+
+  end
+
+  def list_cuisine
+
+  end
+
+  def list_food_category
+
+  end
+
+    def display_meal_details
+
+    end
+
+    def order_now
+
+    end
+
+  end
+
+
+
+
+
+
+
+
 
 
     #* What are you looking for:  (list Type of Meal, Cooking Time, Allergens)
